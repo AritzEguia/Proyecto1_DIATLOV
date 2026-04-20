640f6507dab42521bff5500ec9a70033fdf2b9dd
AritzEguia <aritz_eguia@euneiz.com>
2026-04-20 10:14:02 +0200

    .

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    [Header("Vida")]
+    public float vidaMaxima = 100f;
+    public GameObject enemiePrefab;
+
+    [Header("Seguimiento")]
+    public Transform objetivo;
+    public float velocidad = 5f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (objetivo != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, objetivo.position, velocidad * Time.deltaTime);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        vidaMaxima -= 10;
+        if (vidaMaxima <= 0)
+        {
+            Destroy(enemiePrefab);
+        }
+    }   
+}
